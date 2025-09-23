170983cbc7bcd5467067787f73b3e472446b0fd5
Chau-Nguyen-Developer <chau.nguyen.dev@gmail.com>
2025-09-22 17:58:24 -0700

    Double-checked the info in each file.

--- hello_app/views.py
+++ hello_app/views.py
@@ -8,19 +8,21 @@ def home():
     return render_template("home.html")
 
 @app.route("/about/")
-@app.route("/about")
 def about():
     return render_template("about.html")
 
 @app.route("/contact/")
-@app.route("/contact")
 def contact():
     return render_template("contact.html")
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello_there(name = None):
-    return render_template("hello_there.html", name= name, date = datetime.now)
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
 
 @app.route("/api/data")
 def get_data():
